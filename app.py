c078dbb8ed34b434fc0cc7469a1868068536910c
SeanBel <seanbelavin@gmail.com>
2024-08-16 11:49:10 -0400

    Save Progress Aug 16th 2024

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask, request, jsonify, render_template,redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/',methods=["GET"])
+def get_page():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=3001)
--- static/img/foobar.py
+++
@@ -1,1 +0,0 @@
-print("I cant upload an image until the file has something in it, so I will make this")
