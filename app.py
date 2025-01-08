64cc7169c5a1533237c8dd01514027d8e0d3a3f9
Sean <seanbelavin@gmail.com>
2025-01-08 18:50:53 -0500

    Updated Files

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, request, jsonify, render_template,redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/',methods=["GET"])
-def get_page():
+# Route: Home
+@app.route('/', methods=["GET"])
+def home():
     return render_template("index.html")
+
+# Route: Masterworks
+@app.route('/masterworks', methods=["GET"])
+def masterworks():
+    return render_template("master_works.html")
+
+# Route: About Us
+@app.route('/about', methods=["GET"])
+def about_us():
+    return render_template("about_us.html")
+
+# Route: Submissions
+@app.route('/submissions', methods=["GET"])
+def submissions():
+    return render_template("submit.html")
+
+# Route: Editions
+@app.route('/editions', methods=["GET"])
+def editions():
+    return render_template("editions.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
